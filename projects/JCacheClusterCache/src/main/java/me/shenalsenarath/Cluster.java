a2a16e565b63ad41f32ebc7eca363efea2b3f242
Shenal Senarath <shenalsenarath@gmail.com>
2014-11-14 17:05:25 +0530

    Added JCacheClusterCache- Only implemented the cluster yet

---
+++ projects/JCacheClusterCache/src/main/java/me/shenalsenarath/Cluster.java
@@ -0,0 +1,46 @@
+package me.shenalsenarath;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+/**
+ * Created by shselk on 11/14/2014.
+ */
+public class Cluster {
+    public static void main(String[] args) throws InterruptedException {
+        Cluster app = new Cluster();
+        app.runApp();
+    }
+
+    /**
+     * This will setup two separate hz node instance for clients to join
+     */
+    public void init() {
+        Config config = new Config();
+
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
+        config.getNetworkConfig().setPort(5701);
+        config.getGroupConfig().setName("cluster1");
+        config.getGroupConfig().setPassword("cluster1pass");
+
+        final HazelcastInstance hz1 = Hazelcast.newHazelcastInstance(config);
+
+        Config config2 = new Config();
+
+        config2.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
+        config2.getNetworkConfig().setPort(5702);
+        config2.getGroupConfig().setName("cluster1");
+        config2.getGroupConfig().setPassword("cluster1pass");
+
+        final HazelcastInstance hz2 = Hazelcast.newHazelcastInstance(config2);
+    }
+
+
+
+    public void runApp()
+            throws InterruptedException {
+        init();
+    }
+
+}
