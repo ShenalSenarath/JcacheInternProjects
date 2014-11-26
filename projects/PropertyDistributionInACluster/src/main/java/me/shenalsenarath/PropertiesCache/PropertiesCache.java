95a2b45278866927358689c0724e0bae0b7c2f8f
Shenal Senarath <shenalsenarath@gmail.com>
2014-11-26 12:25:46 +0530

    Changed folder names

--- projects/PropertyDistributionInACluster/src/main/java/me/shenalsenarath/PropertiesCache/PropertiesCache.java
+++ projects/PropertyDistributionInACluster/src/main/java/me/shenalsenarath/PropertiesCache/PropertiesCache.java
@@ -2,6 +2,7 @@ package me.shenalsenarath.PropertiesCache;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
+import javax.cache.Caching;
 import javax.cache.configuration.MutableConfiguration;
 import javax.cache.expiry.AccessedExpiryPolicy;
 import javax.cache.expiry.Duration;
@@ -13,9 +14,14 @@ import javax.cache.spi.CachingProvider;
 public class PropertiesCache {
     CachingProvider cachingProvider;
     CacheManager cacheManager;
-    static final String CACHENAME="PropertiesCache";
+    static final String CACHENAME = "PropertiesCache";
 
-    public Cache initCache(){
+    public PropertiesCache() {
+        cachingProvider = Caching.getCachingProvider();
+        cacheManager = cachingProvider.getCacheManager();
+    }
+
+    public Cache initCache() {
         //configure the cache
         MutableConfiguration<String, String> config = new MutableConfiguration<String, String>();
         config.setStoreByValue(true)
@@ -23,16 +29,28 @@ public class PropertiesCache {
                 .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(Duration.ETERNAL))
                 .setStatisticsEnabled(false);
 
-        cacheManager.createCache(CACHENAME,config);
+        cacheManager.createCache(CACHENAME, config);
 
-        return cacheManager.getCache(CACHENAME,String.class, String.class);
+        return cacheManager.getCache(CACHENAME, String.class, String.class);
     }
 
+    public boolean isInitialized() {
+        try {
+            cacheManager.getCache(CACHENAME, String.class, String.class);
+        } catch (NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
 
+    private Cache getCache() throws NullPointerException{
+        return cacheManager.getCache(CACHENAME, String.class, String.class);
+    }
 
+    public void distroyCache() {
 
-    public void distroyCache(){
-        this.cacheManager.getCache(CACHENAME,String.class, String.class).close();
+        this.cacheManager.getCache(CACHENAME, String.class, String.class).close();
+        this.cacheManager.destroyCache(CACHENAME);
         this.cacheManager.close();
         this.cachingProvider.close();
     }
--- projects/WeblogicClusterJCacheTest/src/main/webapp/cacheput.jsp
+++
@@ -1,8 +0,0 @@
-
-<body>
-    <h2> Put Entry to Cache <h2>
-    <form action="/WeblogicClusterJCacheTest/test/cacheput.do" method="GET">
-    Key: <input type="text" name="key">
-    </form>
-</body>
-</html>
